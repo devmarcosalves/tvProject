1b0a8b1cd06b9134d8ed13ea20c1625ddfd7bfcc
devmarcosalves <marcos.alves05@outlook.com>
2023-02-24 23:08:05 -0300

    third commit
    Implementação do menu de funções da TV utilizando switch/case.

--- src/program/Tv.java
+++ src/program/Tv.java
@@ -30,6 +30,75 @@ public class Tv {
      		   tvq.channel = 1;
      		   tvq.volume = 0;
      	   }
+     	   System.out.println("\n1.Ligar/Desligar 2.Mudar Canal 3.Aumentar Volume \n4.Reduzir Volume 5.Ver Status 6.Voltar");
+     	   System.out.print("Escolha uma opção: ");
+     	   aux = sc.nextInt();
+     	   switch(aux){
+     	   case 1:
+     		   if(opc==1 && tvs.state == false) {
+     			   
+     		   }else if(opc==1 && tvs.state == true) {
+     			   
+     			   System.out.println("A TV da Sala foi desligada!");
+     		   } 
+     		   if(opc==2 && tvq.state == false) {
+     			   
+     		   }else if(opc==2 && tvq.state == true) {
+     			   
+     			   System.out.println("A TV do Quarto foi desligada!");
+     		   }
+     		   break;
+     	   case 2:
+     		   if(opc==1 && tvs.state==true) {
+     			   tvs.channel = tvs.change();
+          		}else if(opc==2 && tvq.state==true){
+          			tvq.channel = tvq.change();
+          		}else {
+          			System.out.println("A TV está desligada!");
+          		}
+     		   	break;
+     	   case 3:
+     		   if(opc==1 && tvs.state==true || opc==2 && tvq.state==true) {
+     			   if(opc==1 && tvs.volume<100) {
+     				   tvs.volume++;
+     				   System.out.println("TV Sala - Volume "+tvs.volume);
+     			   }else if(opc==2 && tvq.volume<100){
+     				   tvq.volume++;
+     				   System.out.println("TV Quarto - Volume "+tvq.volume);
+     			   }else {
+     				   System.out.println("O volume está no limite máximo!");
+     			   }
+     		   }else {
+     			   System.out.println("A TV está desligada!");
+     		   }
+     		   break;
+     	   case 4:
+     		   if(opc==1 && tvs.state==true || opc==2 && tvq.state==true) {
+     			   if(opc==1 && tvs.volume>0) {
+     				   tvs.volume--;
+     				   System.out.println("TV Sala - Volume "+tvs.volume);
+     			   }else if(opc==2 && tvq.volume>0) {
+     				   tvq.volume--;
+     				   System.out.println("TV Quarto - Volume "+tvq.volume);
+     			   }else {
+     				   System.out.println("O volume está zerado!");
+     			   }
+     		   }else {
+     			   System.out.println("A TV está desligada!");
+     		   }
+     		   break;
+     	   case 5:
+     		   System.out.println((tvs.state) ? "A TV da Sala está ligada no canal "+tvs.channel+" e no volume "+tvs.volume : "A TV da Sala está desligada no canal "+tvs.channel+" e no volume "+tvs.volume);
+     		   System.out.println((tvq.state) ? "A TV do Quarto está ligada no canal "+tvq.channel+" e no volume "+tvq.volume : "A TV do Quarto está desligada no canal "+tvq.channel+" e no volume "+tvq.volume);
+     		   break;
+     	   case 6:
+     		   System.out.print("\n");
+     		   main(null);
+     		   break;
+     	   default:
+     		   System.out.println("Opção Inválida!");
+     		   break;
+     	   }
         }   
 	}
 	public void on() {
